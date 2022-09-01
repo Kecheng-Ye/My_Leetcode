d1d70a6980f21f12689352767008290e50e07fa7
Kecheng-Ye <kevin_ye1997@outlook.com>
2022-09-01 11:42:15 -0700

    add: 9/1-11:40AM

--- Problem/By section/Greedy/Problem 253/problem_253.cpp
+++ Problem/By section/Greedy/Problem 253/problem_253.cpp
@@ -4,43 +4,38 @@ using namespace std;
 
 class Solution {
 public:
-    static bool compare(const vector<int>& a, const vector<int>& b) {
-        if(a[1] == b[1]) return a[0] < b[0];
-        
-        return a[1] < b[1];
-    }
-    
-    // Time: O(nlogn), Space: O(n)
     int minMeetingRooms(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
-        
-        vector<int> cur_rooms;
-        
-        for(const auto& interval : intervals) {
-            int start = interval[0];
-            int end = interval[1];
-            bool check_room = false;
-            
-            for(auto& room : cur_rooms) {
-                if(start >= room) {
-                    check_room = true;
-                    room = end;
-                    break;
-                }
-            }
-            
-            if(!check_room) {
-                cur_rooms.push_back(end);
-            }
+        vector<int> start(n);
+        vector<int> end(n);
+
+        int idx = 0;
+        for (const auto& meeting : intervals) {
+            start[idx] = meeting[0];
+            end[idx] = meeting[1];
+            idx++;
         }
-        
-        return cur_rooms.size();
+
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+
+        int usedRoom = 0;
+        int endPtr = 0;
+        for (int i = 0; i < n; i++) {
+            if (start[i] >= end[endPtr]) {
+                usedRoom -= 1;
+                endPtr += 1;
+            }
+
+            usedRoom += 1;
+        }
+
+        return usedRoom;
     }
 };
 
 int main(int argc, char** argv) {
     Solution s;
-    
+
     return 0;
 }
--- Problem/By section/Greedy/Problem 45/problem_45.cpp
+++ Problem/By section/Greedy/Problem 45/problem_45.cpp
@@ -10,22 +10,23 @@ public:
     int jump(vector<int>& nums) {
         int jump = 0, current = 0, n = nums.size();
         int farthest = -1;
-        
+
         for(int i = 0; i < n - 1; i++) {
             farthest = max(farthest, i + nums[i]);
-            
+
             if(i == current) {
                 jump++;
                 current = farthest;
+                if (current >= n - 1) return jump;
             }
         }
-        
-        return jump;
+
+        return -1;
     }
 };
 
 int main(int argc, char** argv) {
     Solution s;
-    
+
     return 0;
 }
